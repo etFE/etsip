791147938ce1cc09d3fdb4d061925413b496b957
wsj <wsj_simon@163.com>
2018-06-06 16:28:36 +0800

    update the dictionary tip modal

--- src/routes/Dictionary/index.js
+++ src/routes/Dictionary/index.js
@@ -1,17 +1,19 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'dva';
-import { Card } from 'antd';
+import { Card, Modal, Icon } from 'antd';
 import EditableTable from 'components/Table/EditableTable.js'
 import PageHeaderLayout from '../../layouts/PageHeaderLayout';
+import styles from './dictionary.less'
 
 @connect(({ dictionary, loading }) => ({
     dictionary,
     loading: loading.models.dictionary,
 }))
-export default class ReportLayout extends PureComponent {
+export default class DictionaryContent extends PureComponent {
     constructor(props) {
         super(props);
         this.state = {
+            dictionaryTipShow: false,
             columns: [
                 {
                     title: '字典编码',
@@ -31,7 +33,16 @@ export default class ReportLayout extends PureComponent {
                     },
                 },
                 {
-                    title: '字典SQL',
+                    title: (
+                        <span>
+                            字典SQL
+                            <Icon
+                                type="question-circle"
+                                className={styles.tipIcon}
+                                onClick={this.showDictionaryTip}
+                            />
+                        </span>
+                    ),
                     dataIndex: 'dictBody',
                     editor: {
                         type: 'textarea',
@@ -40,10 +51,19 @@ export default class ReportLayout extends PureComponent {
             ],
         }
     }
-
-
     componentDidMount () {
         this.queryData()
+    }
+
+    showDictionaryTip = () => {
+        this.setState({
+            dictionaryTipShow: true,
+        })
+    }
+    hideDictionaryTip = () => {
+        this.setState({
+            dictionaryTipShow: false,
+        })
     }
 
     queryData = () => {
@@ -103,6 +123,36 @@ export default class ReportLayout extends PureComponent {
                         deleteData={this.deleteData}
                         changeCell={this.changeCell}
                     />
+                    <Modal
+                        title="字典SQL编写 提示"
+                        footer={null}
+                        className={styles.tipModal}
+                        visible={this.state.dictionaryTipShow}
+                        onCancel={this.hideDictionaryTip}
+                    >
+                        <article>
+                            <h3>提示</h3>
+                            <p>
+                                1、SQL中的字段必须要有<code>value</code>和<code>text</code>
+                                来适配下拉框。可以通过重命名的方式。
+                            </p>
+                            <p>
+                                2、如果想要下拉框可以后台查询，那么需要手动写查询条件
+                            </p>
+                            <h3>例子</h3>
+                            <pre>
+                                <code>{
+                                    `select
+    id value,username text
+from
+    sys_user
+where
+    username like @key '%'`}
+                                </code>
+                            </pre>
+                        </article>
+
+                    </Modal>
                 </Card>
             </PageHeaderLayout>
         )
